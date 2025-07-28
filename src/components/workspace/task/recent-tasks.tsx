11ae1a9c01502ae1c38abbb38d8322977ed38713
SidraAkhter <sidraakhter16@gmail.com>
2025-07-29 01:31:49 +0500

    Fix Badge default import

--- src/components/workspace/task/recent-tasks.tsx
+++ src/components/workspace/task/recent-tasks.tsx
@@ -1,5 +1,5 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { Badge } from "@/components/ui/badge";
+import  Badge  from "@/components/ui/badge";
 import { TaskPriorityEnum, TaskStatusEnum } from "@/constant";
 import useWorkspaceId from "@/hooks/use-workspace-id";
 import { getAllTasksQueryFn } from "@/lib/api";
