75783a26e730472ee8b91b710b7977c941a74d92
SidraAkhter <sidraakhter16@gmail.com>
2025-08-02 19:47:44 +0500

    initial commit

--- src/lib/api/logout.ts
+++
@@ -1,8 +0,0 @@
-import API from "./api";
-
-const logoutMutationFn = async () => {
-  const response = await API.post("/auth/logout");
-  return response.data;
-};
-
-export default logoutMutationFn;
